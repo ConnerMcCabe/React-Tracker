36b477c9b7afa0e19c84096870e7f5a275559890
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-08 18:01:04 -0500

    adding styling to card component

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./Cards.module.css"
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
   console.log(confirmed)
@@ -12,33 +13,33 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Infected</Typography>
             <Typography variant="h5">
               <CountUp start={0} end={confirmed.value} duration={2.5} separator=',' />
             </Typography>
-            <Typography color="textSecondary">Dummy Date</Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="h5">Active Cases</Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Recovered</Typography>
             <Typography variant="h5">
               <CountUp start={0} end={recovered.value} duration={2.5} separator=',' />
             </Typography>
-            <Typography color="textSecondary">Dummy Date</Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="h5">Recovered Cases</Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Deaths</Typography>
             <Typography variant="h5">
               <CountUp start={0} end={deaths.value} duration={2.5} separator=',' />
             </Typography>
-            <Typography color="textSecondary">Dummy Date</Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="h5">Death count</Typography>
           </CardContent>
         </Grid>
