e3442d950782eb52e4b74cbf2adcc802be8607df
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-08 17:22:58 -0500

    adding to card component

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import styles from "./Cards.module.css"
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 const Cards = (props) => {
   console.log(props)
   
   return (
-
-    <h1>Cards</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography variant="h5">Dummy Data</Typography>
+            <Typography color="textSecondary">Dummy Date</Typography>
+            <Typography variant="h5">Active Cases</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+            <Typography variant="h5">Dummy Data</Typography>
+            <Typography color="textSecondary">Dummy Date</Typography>
+            <Typography variant="h5">Recovered Cases</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+            <Typography variant="h5">Dummy Data</Typography>
+            <Typography color="textSecondary">Dummy Date</Typography>
+            <Typography variant="h5">Death count</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
