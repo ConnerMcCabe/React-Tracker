5c444ebc33d61988a5554ec0ba6cf052e789f7d4
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-08 15:06:55 -0500

    importing components to app.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Cards from '../Components/Cards/Cards'
+import Chart from '../Components/Chart/Chart'
+import CountryPicker from '../Components/CountryPicker/CountryPicker'
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
   );
 }
