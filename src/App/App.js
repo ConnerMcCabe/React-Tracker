bc6bd58d47980b8033982d8df9c09a12ee6c99e5
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-08 15:37:17 -0500

    getting api response and console logging data

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
 import { Cards, Chart, CountryPicker } from '../Components/';
+import { fetchData } from '../api';
 
-function App() {
-  return (
-    <div className="App">
-      <Cards />
-      <CountryPicker />
-      <Chart />
-    </div>
-  );
+class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
