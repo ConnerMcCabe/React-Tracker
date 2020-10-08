d062cf50235a169b718a02c86ed3dc48e7ae4dc4
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-08 16:25:35 -0500

    adding to card component and streamlining api calls

--- src/App/App.js
+++ src/App/App.js
@@ -4,16 +4,20 @@ import { Cards, Chart, CountryPicker } from '../Components/';
 import { fetchData } from '../api';
 
 class App extends React.Component {
-
+  state = {
+    data: {},
+  }
   async componentDidMount() {
-    const data = await fetchData();
-    console.log(data)
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData })
   }
 
   render() {
+    const { data } = this.state;
+    
     return (
       <div className="App">
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
       </div>
