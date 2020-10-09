9618d6c50c4e1c59b5446d19cdd5fc086980c58b
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-09 14:54:28 -0500

    displaying daily data on chart, adding additional api for tracking

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 
 const Chart = () => {
-  const [dailyData, setDailyData] = useState({});
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     const fetchApi = async () => {
@@ -12,21 +12,20 @@ const Chart = () => {
     }
     console.log(dailyData)
     fetchApi();
-  }, []);
+  });
 
   const lineChart = (
-    dailyData[0] 
-    ? (
+    dailyData[0] ? (
       <Line 
         data={{
-          labels: dailyData(({ date }) => date),
+          labels: dailyData.map(({ date }) => date),
           datasets: [{
-            data: dailyData(({ confirmed }) => confirmed),
+            data: dailyData.map(({ confirmed }) => confirmed),
             label: "Infected",
             borderColor: "#3333ff",
             fill: true,
           }, {
-            data: dailyData(({ deaths }) => deaths),
+            data: dailyData.map(({ deaths }) => deaths),
             label: "Deaths",
             borderColor: "red",
             backgroundColor: 'rgba(255, 0, 0, 0.5)',
@@ -38,8 +37,8 @@ const Chart = () => {
   ); 
 
   return (
-    <div className={styles.Chart}>
-
+    <div className={styles.container}>
+      {lineChart}
     </div>
   )
 }
