c57e46560ff0ecf2e544fd53e0f9af4e9a47f542
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-12 15:30:05 -0500

    adding to country picker component

--- src/Components/CountryPicker/CountryPicker.jsx
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { }
+import { NativeSelect, FormControl } from '@material-ui/core'
+import styles from "./CountryPicker.module.css"
 
 const CountryPicker = () => {
   return (
-    
+    <FormControl classnName={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+      </NativeSelect>
+    </FormControl>
   )
 }
 
