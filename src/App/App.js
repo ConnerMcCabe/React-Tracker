525b2047e30237df454b09e9c51d17f026cbab61
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-14 14:04:04 -0500

    finishing bar chart for individual country data

--- src/App/App.js
+++ src/App/App.js
@@ -19,13 +19,13 @@ class App extends React.Component {
   }
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className="App">
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Chart />
+        <Chart data={data} country={country}/>
       </div>
     )
   }
