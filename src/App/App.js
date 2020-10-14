daa575b4e989c4e6aff3e5d5150d75585d7c356c
Conner McCabe <conner.p.mccabe@gmail.com>
2020-10-14 13:41:25 -0500

    cards dynamically displaying data

--- src/App/App.js
+++ src/App/App.js
@@ -10,13 +10,12 @@ class App extends React.Component {
   }
   async componentDidMount() {
     const fetchedData = await fetchData();
-    this.setState({ data: fetchedData })
+    this.setState({ data: fetchedData });
   }
 
   handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
-
-    console.log(fetchedData);
+    this.setState({ data: fetchedData, country: country });
   }
 
   render() {
